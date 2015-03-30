cf870e72422e9a8aa92a44757160edde70bcd675
rameshsunkari <rameshsunkari@gmail.com>
2015-03-30 12:30:41 -0400

    Avoided the warnings

--- DesignPatternsDemo/src/org/thinkadv/dp/behavioural/chain/ChainDPDemo.java
+++ DesignPatternsDemo/src/org/thinkadv/dp/behavioural/chain/ChainDPDemo.java
@@ -6,19 +6,22 @@ public class ChainDPDemo {
 
 	public static void main(String[] args) {
 		AtmDispenseChain atmDispenser = new AtmDispenseChain();
+		Scanner input = null;
 		int amount = 100;
         while (amount >= 100) {
         	System.out.println("Enter amount to dispense");
-            Scanner input = new Scanner(System.in);
+            input = new Scanner(System.in);
             amount = input.nextInt();
             if (amount % 10 != 0) {
                 System.out.println("Amount should be in multiple of 10s.");
-                return;
+                System.exit(0);;
             }
             // process the request
             atmDispenser.getRootChain().dispense(new Currency(amount));
         }
-
+        
+        if (input != null)
+        	input.close();
 	}
 
 }
