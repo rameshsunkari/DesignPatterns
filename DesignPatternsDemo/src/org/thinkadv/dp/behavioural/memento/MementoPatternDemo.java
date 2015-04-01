7ecbcf2858d6490af74432142ea753923f2118f1
rameshsunkari <rameshsunkari@gmail.com>
2015-04-01 06:09:50 -0400

    Memento Pattern Commit

---
+++ DesignPatternsDemo/src/org/thinkadv/dp/behavioural/memento/MementoPatternDemo.java
@@ -0,0 +1,28 @@
+package org.thinkadv.dp.behavioural.memento;
+
+public class MementoPatternDemo {
+	public static void main(String[] args) {
+		// caretaker
+		DietInfoCaretaker dietInfoCaretaker = new DietInfoCaretaker();
+
+		// originator
+		DietInfo dietInfo = new DietInfo("Fred", 1, 100);
+		System.out.println(dietInfo);
+
+		dietInfo.setDayNumberAndWeight(2, 99);
+		System.out.println(dietInfo);
+
+		System.out.println("Saving state.");
+		dietInfoCaretaker.saveState(dietInfo);
+
+		dietInfo.setDayNumberAndWeight(3, 98);
+		System.out.println(dietInfo);
+
+		dietInfo.setDayNumberAndWeight(4, 97);
+		System.out.println(dietInfo);
+
+		System.out.println("Restoring saved state.");
+		dietInfoCaretaker.restoreState(dietInfo);
+		System.out.println(dietInfo);
+	}
+}
